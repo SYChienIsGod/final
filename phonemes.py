69075341d1fb5c92f02462ed6157a7817041b51c
PHHung <phhung@media.ee.ntu.edu.tw>
2015-06-25 10:17:22 +0800

    support phone 60 to 39

--- phonemes.py
+++ phonemes.py
@@ -8,7 +8,7 @@ Created on Fri Jun 12 13:13:22 2015
 import numpy
 
 
-phonMapP='../data/conf/phones.60-48-39.map'
+phonMapP='../../data/conf/phones.60-48-39.map'
 
 # number to phoneme
 c60=dict()
@@ -20,6 +20,7 @@ ph60 = dict()
 ph48=dict()
 ph39=dict()
 ph60_ph48=dict()
+ph60_ph39=dict()
 
 
 with open(phonMapP) as f:
@@ -33,9 +34,11 @@ with open(phonMapP) as f:
                 ph48[data[1]] = len(ph48)
                 c48[ph48[data[1]]]=data[1]
             ph60_ph48[data[0]]=data[1]
-        if len(data) > 2 and not ph39.has_key(data[2]):
-            ph39[data[2]] = len(ph39)
-            c39[ph39[data[2]]]=data[2]
+        if len(data) > 2 :
+            if not ph39.has_key(data[2]):
+                ph39[data[2]] = len(ph39)
+                c39[ph39[data[2]]]=data[2]
+            ph60_ph39[data[0]]=data[2]
 
 def trans_ph60_ph48(d):
     if type(d) == list:
@@ -45,7 +48,16 @@ def trans_ph60_ph48(d):
         return r
     else:
         return ph60_ph48[d]
-            
+
+def trans_ph60_ph39(d):
+    if type(d) == list:
+        r = list()
+        for w in d:
+            r.append(trans_ph60_ph39(w))
+        return r
+    else:
+        return ph60_ph39[d]
+
 def trans_ph48(d):
     if type(d) == list:
         r = list()
@@ -60,6 +72,20 @@ def trans_ph48(d):
     else:
         return ph48[d]
 
+def trans_ph39(d):
+    if type(d) == list:
+        r = list()
+        for w in d:
+            r.append(trans_ph39(w))
+        return r
+    elif type(d) == numpy.ndarray:
+        r = numpy.zeros(shape=(d.shape),dtype='int')
+        for i in range(d.size):
+            r[i]=trans_ph39(d[i])
+        return r
+    else:
+        return ph39[d]
+
 def trans_c48(d):
     if type(d) == list:
         r = list()
@@ -68,4 +94,13 @@ def trans_c48(d):
         return r
     else:
         return c48[d]
+
+def trans_c39(d):
+    if type(d) == list:
+        r = list()
+        for w in d:
+            r.append(trans_c39(w))
+        return r
+    else:
+        return c39[d]
 
